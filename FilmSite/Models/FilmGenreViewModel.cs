f34f2ac9dd0420a42ac780218d4a80e794c22096
anontyro <alexwilkinson@gmail.com>
2017-04-04 00:49:16 +0800

    Added search features by genre

---
+++ FilmSite/Models/FilmGenreViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace FilmSite.Models
+{
+    public class FilmGenreViewModel
+    {
+        public List<Movie> movies;
+        public SelectList genres;
+        public string movieGenre { get; set; }
+    }
+}
--- FilmSite/Models/Movie.cs
+++ FilmSite/Models/Movie.cs
@@ -1,16 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace FilmSite.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+
+        [Display(Name = "Film Title")]
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Display(Name ="US Box Office Gross")]
         public decimal Price { get; set; }
 
     }
